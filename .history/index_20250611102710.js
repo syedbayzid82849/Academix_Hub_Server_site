90d94dd1b1c5989d1a5452c72d83bb766ccc99e3
syed bayzid <husains82849@gmail.com>
2025-06-11 10:28:57 +0600

    the get operation is make on '/all-course' for find alll courses

--- .history/index_20250611102710.js
+++ .history/index_20250611102710.js
@@ -35,6 +35,7 @@ async function run() {
             res.send(courses);
         });
 
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
