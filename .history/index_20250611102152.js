0b03364b4765fc6a9083e67fe1dd777f453f5c08
syed bayzid <husains82849@gmail.com>
2025-06-11 10:24:09 +0600

    in server port 3000 is showed the 'hello world'

--- .history/index_20250611102152.js
+++ .history/index_20250611102152.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
 
 
-
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
