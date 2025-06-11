ce1e5abcf14bda23548e55e80846398ab0ebd400
syed bayzid <husains82849@gmail.com>
2025-06-11 10:25:13 +0600

    mongoDB is setuped

--- .history/index_20250611102419.js
+++ .history/index_20250611102419.js
@@ -9,17 +9,6 @@ const port = process.env.PORT || 3000;
 
 
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.MONGODB_URI, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-
-
-
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
--- .history/index_20250611102438.js
+++ .history/index_20250611102438.js
@@ -5,7 +5,8 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
+app.use(express.json());
 
 
 
